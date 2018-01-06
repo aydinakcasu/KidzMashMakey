e5e38f186ed4934f368be999d0c4e0349c289e96
Aydin Akcasu <AydinAkcasu@quickenloans.com>
2018-01-06 13:39:48 -0500

    Add shuffle tools.

--- tools/tools.js
+++ tools/tools.js
@@ -28,3 +28,21 @@ function listToDictionary(list) {
 }
 
 Array.prototype.listToDictionary = listToDictionary;
+
+
+// ---------------------------------------------
+
+/**
+ * Randomize array element order in-place.
+ * Using Durstenfeld shuffle algorithm.
+ */
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
