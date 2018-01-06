29aa5ab11b1f1fce0fecd27779b011fff03c4ce7
Aydin Akcasu <AydinAkcasu@quickenloans.com>
2018-01-06 13:38:49 -0500

    Add dump() tools.

--- tools/tools.js
+++ tools/tools.js
@@ -1,3 +1,11 @@
+// ---------------------------------------------
+function dump(obj) {
+    obj = obj || this || {};
+    return JSON.stringify(obj, null, '\t');
+}
+Array.prototype.dump = dump;
+Object.prototype.dump = dump;
+
 function listToDictionary(list) {
     list = list || this || [];
     var dictionary = {};
